06872cfdf6c3034dfd85d788c9c272c53ee19d87
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-06-28 16:55:26 -0300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::thread;
+use std::time::Duration;
+
+use embedded_hal::digital::v2::OutputPin;
+use esp_idf_hal::peripherals::Peripherals;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = peripherals.pins.gpio4.into_output()?;
+
+    loop {
+        led.set_high()?;
+        // we are using thread::sleep here to make sure the watchdog isn't triggered
+        thread::sleep(Duration::from_millis(1000));
+
+        led.set_low()?;
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
