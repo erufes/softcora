c0e1ed2bb8289fc8e7d1f3681b813f2034b5c013
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-07-12 18:57:23 -0300

    Revert "Adição de mais precauções de segurança associadas ao wireless"
    This reverts commit a2b0a81b4c00c16c0bc674bf219e2f8c2a11c5e7.

--- env.sh
+++ env.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
-
-# EDIT THIS SECTION
-SSID="ssid"
-PASSWORD="password"
-
-
-##################################
-# DO NOT EDIT BELOW THIS LINE!
-##################################
-RED="\e[31m"
-ENDCOLOR="\e[0m"
-GREEN="\e[32m"
-
-if [[ "$SSID" == "" ]]; then
-    echo -e "${RED}[ERROR]${ENDCOLOR} Please setup SSID at env.sh"
-    exit 1
-fi
-
-if [[ "$PASSWORD" == "" ]]; then
-    echo -e "${RED}[ERROR]${ENDCOLOR} Please setup PASSWORD at env.sh"
-    exit 1
-fi
-
-export WIFI_SSID="$SSID"
-export WIFI_PASSWORD="$PASSWORD"
-
-echo -e "${GREEN}[SUCCESS]${ENDCOLOR} Wireless settings set."
+export WIFI_SSID=\"ssid\"
+export WIFI_PASSWORD=\"password\"
--- include/env.h
+++ include/env.h
@@ -5,13 +5,13 @@
 
 #ifndef WIFI_PASSWORD
 #warning                                                                       \
-    "WIFI_PASSWORD not defined, using default value. Please export it from CLI by running "source env.sh" before build."
+    "WIFI_PASSWORD not defined, using default value. Please export it from CLI before build."
 #define WIFI_PASSWORD "default password"
 #endif
 
 #ifndef WIFI_SSID
 #warning                                                                       \
-    "WIFI_SSID not defined, using default value. Please export it from CLI by running "source env.sh" before build."
+    "WIFI_SSID not defined, using default value. Please export it from CLI before build."
 #define WIFI_SSID "default ssid"
 #endif
 
