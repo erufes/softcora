465e3e91b81d4322e752498767f96492e76b61b1
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-07-12 17:24:13 -0300

    Instanciação do ERUSBot agora usa os parâmetros corretos no struct

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 #include "pins.h"
 
 static ERUSBot bot = ERUSBot(
-    {Pins::Engine::left::power, Pins::Engine::left::direction::forward},
-    {Pins::Engine::right::power, Pins::Engine::right::direction::forward},
+    {Pins::Engine::left::power, Pins::Engine::left::direction::forward,
+     Pins::Engine::left::direction::backward},
+    {Pins::Engine::right::power, Pins::Engine::right::direction::forward,
+     Pins::Engine::right::direction::backward},
     Pins::sensors);
 
 void setup() {}
