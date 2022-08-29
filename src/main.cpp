1b78d8b72f558956dcc53b2db235e406b2d309b0
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-08-29 10:37:17 -0300

    Limpeza da main. Teste do webhook do discord

--- src/main.cpp
+++ src/main.cpp
@@ -36,55 +36,6 @@ void nextFlag() { dirFlags = (dirFlags + 1) % 4; }
 // static Sensor s = Sensor(32);
 
 void loop() {
-    // pinMode(32, OUTPUT);
-    // digitalWrite(32, HIGH);
-    // int64_t now = esp_timer_get_time();
-    // pinMode(32, INPUT);
-    // while (1) {
-    //     if (digitalRead(32) == LOW || esp_timer_get_time() - now > timeout)
-    //         break;
-    // }
-
-    // int64_t diff = esp_timer_get_time() - now;
-    // float percent = float(diff) / 1024;
-    // printf("diff: %.2f\%\n", percent);
-
-    // s.read();
-
-    // Engine* mot1 = bot->getMotor1();
-    // Engine* mot2 = bot->getMotor2();
-    // currentTime = esp_timer_get_time();
-    // int64_t delta = currentTime - lastTime;
-    // // if (delta > 2000000) {
-    // if (delta > 200000) {
-    //     lastTime = currentTime;
-    //     nextFlag();
-    //     printf("battery reads %.2f\n", bot->getBatteryVoltage());
-    //     printf("raw battery reads %d\n", bot->getRawBatteryVoltage());
-
-    //     switch (dirFlags) {
-    //     case 0:
-    //         // printf("setting speed to 0\n");
-    //         mot1->setSpeed(0);
-    //         mot2->setSpeed(0);
-    //         break;
-    //     case 1:
-    //         // printf("setting speed to 255\n");
-    //         mot1->setSpeed(255);
-    //         mot2->setSpeed(255);
-    //         break;
-    //     case 2:
-    //         // printf("setting speed to -255\n");
-    //         mot1->setSpeed(-255);
-    //         mot2->setSpeed(-255);
-    //         break;
-    //     case 3:
-    //         // printf("setting speed to 100\n");
-    //         mot1->setSpeed(100);
-    //         mot2->setSpeed(100);
-    //         break;
-    //     }
-    // }
     // Motor testing
     {
         Engine* mot1 = bot->getMotor1();
