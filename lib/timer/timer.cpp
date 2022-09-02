e1527f6a6b2b87f6b359edcedbac2468eca4ec7f
Thiago Tineli Paiva <thiago.t_27@hotmail.com>
2022-09-02 15:51:11 -0300

    Timers Adicionados

--- lib/timer/timer.cpp
+++ lib/timer/timer.cpp
@@ -1,14 +1,59 @@
 #include "timer.h"
 
-Timer::Timer(uint duration) {}
-Timer::Timer(uint duration, std::function<void()> callback) {}
-void Timer::start() {}
-void Timer::stop() {}
-void Timer::reset() {}
-void Timer::onFinish(std::function<void()> callback) {}
-bool Timer::isDone() {}
-void Timer::tick() {}
-void Timer::setDuration(uint duration) {}
-uint Timer::getDuration() {}
-uint Timer::getRemainingTime() {}
-uint Timer::getElapsedTime() {}
+Timer::Timer(uint duration) {
+    this->duration = duration;
+    this->elapsedTime = 0;
+    this->isRunning = false;
+    this->done = false;
+}
+Timer::Timer(uint duration, std::function<void()> callback) {
+    this->duration = duration;
+    this->elapsedTime = 0;
+    this->isRunning = false;
+    this->callback = callback;
+    this->done = false;
+}
+void Timer::start() {
+    this->isRunning = true;
+    this->startTime = esp_timer_get_time();
+}
+void Timer::stop() {
+    this->isRunning = false;
+    this->elapsedTime = esp_timer_get_time() - startTime;
+}
+void Timer::reset() {
+    this->startTime = duration + esp_timer_get_time();
+    this->done = false;
+}
+
+void Timer::onFinish(std::function<void()> callback) {
+    if(isDone()){
+        callback();
+    }
+}
+
+bool Timer::isDone() {
+    if(this->done && this->isRunning){
+        return true;
+    }
+    return false;
+}
+
+void Timer::tick() {
+    if (isRunning && (esp_timer_get_time() - startTime) > duration-elapsedTime){
+        done = true;
+    }
+}
+
+void Timer::setDuration(uint duration) {
+    this->duration = duration;
+}
+uint Timer::getDuration() {
+    return duration;
+}
+uint Timer::getRemainingTime() {
+    return this->duration - getElapsedTime();
+}
+uint Timer::getElapsedTime() {
+    return (esp_timer_get_time() - startTime);
+}
